9a6008d406a60ff0a6594520adc2a914f2d298cc
ohjust <84974475+ohjust@users.noreply.github.com>
2025-07-31 14:58:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const { exec, execSync } = require('child_process');
 
 // 环境变量
 const UUID = process.env.UUID || 'a2056d0d-c98e-4aeb-9aab-37f64edd5710'; // 使用哪吒v1，在不同的平台部署需修改UUID，否则会覆盖
-const NEZHA_SERVER = process.env.NEZHA_SERVER || '';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
-const NEZHA_PORT = process.env.NEZHA_PORT || '';           // 哪吒v1没有此变量，v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
-const NEZHA_KEY = process.env.NEZHA_KEY || '';             // v1的NZ_CLIENT_SECRET或v0的agent端口  
+const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezhak.5201972.xyz';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
+const NEZHA_PORT = process.env.NEZHA_PORT || '443';           // 哪吒v1没有此变量，v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
+const NEZHA_KEY = process.env.NEZHA_KEY || '6ItGHErK9kbI0RBr1e';             // v1的NZ_CLIENT_SECRET或v0的agent端口  
 const AUTO_ACCESS = process.env.AUTO_ACCESS || false;      // 是否开启自动访问保活,false为关闭,true为开启,需同时填写DOMAIN变量
 const XPATH = process.env.XPATH || UUID.slice(0, 8);       // xhttp路径,自动获取uuid前8位
 const SUB_PATH = process.env.SUB_PATH || 'sub';            // 节点订阅路径
-const DOMAIN = process.env.DOMAIN || '';                   // 域名或ip,留空将自动获取服务器ip
-const NAME = process.env.NAME || 'Vls';                    // 节点名称
+const DOMAIN = process.env.DOMAIN || 'btpp03.btpp.hf.space';                   // 域名或ip,留空将自动获取服务器ip
+const NAME = process.env.NAME || 'huggingface';                    // 节点名称
 const PORT = process.env.PORT || 3000;                     // http服务
 
 // 核心配置
